6c0a04486763bb3f7e2c75d74640bce4dc66d870
S Takahashi <samonikura100@gmail.com>
2020-07-30 02:15:10 +0900

    pushテスト

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -9,11 +9,4 @@ describe('HelloWorld.vue', () => {
     })
     expect(wrapper.text()).toMatch(msg)
   })
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch('abcdefg')
-  })
 })
